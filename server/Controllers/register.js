cefdff6ccb075271f149195fb4fc28112a471cb2
Jayy <cham.voravee@gmail.com>
2023-12-14 17:27:25 +0700

    create register DB

--- server/Controllers/register.js
+++ server/Controllers/register.js
@@ -1,16 +1,11 @@
-//const db =require('../Models/register.js')
+const db = require('../Models/register.js')
 
 exports.register = async(req, res) => {
     try {
-        //const collection = db.collection("information")
-
-        //const user = {name:req.body.name}
-        //const userData = {...req.body}
-        //const regis = await collection.insertOne(userData)
-
         console.log(req.body)
+        const register = await db(req.body).save()
+        res.send(register)
         
-        res.send('Register success')
     } catch (err) {
         //error
         console.log(err)
